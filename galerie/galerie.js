2044c9bd1b104bb01fed792ddfa695d3d0e098a1
DoggoKt <mr.doge2019@gmail.com>
2024-10-03 14:37:33 +0200

    Fix empty elements when scrolling gallery + add captions

--- galerie/galerie.js
+++ galerie/galerie.js
@@ -1,5 +1,5 @@
 const FIREBASE_BASEURL = "https://europe-west4-pristine-sphere-435312-g4.cloudfunctions.net/"
-const IMAGE_TEMPLATE = `<a href="{0}", title="{1}"><img src={0} alt="{1}" /></a>`;
+const IMAGE_TEMPLATE = `<a href="{0}" title="{1}"><img src={0} alt="{2}" /></a>`;
 
 let currentHeading = "";
 const images = fetch(FIREBASE_BASEURL + "getImages?getlinks=true").then(res => res.json()).then(data => {
@@ -8,18 +8,17 @@ const images = fetch(FIREBASE_BASEURL + "getImages?getlinks=true").then(res => r
 
     for (const image of data) {
         if (image.heading !== currentHeading) {
-            if (currentHeading !== ""){
-                container.innerHTML += "<br/>"
-            }
             currentHeading = image.heading;
             container.innerHTML += `<h2>${currentHeading}</h2>`
         }
         container.innerHTML += IMAGE_TEMPLATE
             .replaceAll("{0}", image.link)
-            .replaceAll("{1}", new URL(decodeURIComponent(image.link)).pathname.split("/").pop()) + "<br/>";
+            .replaceAll("{1}", image.heading)
+            .replaceAll("{2}", new URL(decodeURIComponent(image.link)).pathname.split("/").pop());
     }
 
     lightGallery(document.getElementById("gallery_container"),{
-        plugins: [lgVideo]
+        plugins: [lgVideo],
+        selector: "a"
     })
 });
