89d7c3ab3d67a4e92322a91dc9d1229b7f5b3596
DoggoKt <mr.doge2019@gmail.com>
2024-12-18 08:41:32 +0100

    Add shooters table

--- tabulka/tabulka.js
+++ tabulka/tabulka.js
@@ -11,6 +11,15 @@ const TEAM_TEMPLATE =
 <td class="points" style="font-weight: bold">{POINTS}</td>
 </tr>`;
 
+const SHOOTER_TEMPLATE = `
+<tr>
+<td style="font-weight: bold">{POS}</td>
+<td>{SHOOTER}</td>
+<td>{GOALS}</td>
+<td>{ASSISTS}</td>
+</tr>`
+
+
 const GREEN_MAX = 4
 const YELLOW_MAX = 12
 
@@ -31,7 +40,7 @@ async function loadTeams() {
             .replaceAll("{POINTS}", team.points);
     });
 
-    const container = document.querySelector("#table-container .body");
+    const container = document.querySelector("#table-container .table.teams .body");
 
     if (elements.length === 0) {
         container.innerHTML = `<h3 style="text-align: center">Ještě nejsou odehrané žádné zápasy.</h3>`;
@@ -40,4 +49,49 @@ async function loadTeams() {
     }
 }
 
-loadTeams().then(() => console.log("Teams loaded."))
+async function loadShooters() {
+    const data = await fetch(FIREBASE_BASEURL + "getMatches").then(r => r.json());
+
+    const counted = {};
+
+    for (const match of Object.values(data)){
+        if (!match.events) continue;
+        for (const event of [...match.events.left?.split("\n")||[], ...match.events.right?.split("\n")||[]]){
+            if (event.length < 1) continue;
+            if (!event.includes("⚽")) continue;
+            const splitted = event.split("(").map(e => e.replaceAll("⚽", "").trim())
+
+            console.log(match)
+            counted[splitted[0]] ??= {goals: 0, assists: 0/*, team: match.events.left.includes(event) ? match.team_left : match.team_right*/}
+            counted[splitted[0]].goals++;
+
+            if (splitted.length > 1) {
+                const assistName = splitted[1].slice(0,-1);
+                counted[assistName] ??= {goals: 0, assists: 0}
+                counted[assistName].assists++
+            }
+        }
+    }
+
+    console.log(counted)
+
+    const elements = Object.keys(counted).sort((a,b) => (counted[b].goals - counted[a].goals) || (counted[b].assists - counted[a].assists)).slice(0,30).map((plr, idx) => {
+      return SHOOTER_TEMPLATE
+          .replaceAll("{POS}", String(idx+1))
+          .replaceAll("{SHOOTER}", `${plr}`)
+          .replaceAll("{GOALS}", counted[plr].goals)
+          .replaceAll("{ASSISTS}", counted[plr].assists)
+    })
+
+    const container = document.querySelector("#table-container .table.shooters .body");
+
+    if (elements.length === 0) {
+        container.innerHTML = `<h3 style="text-align: center">Ještě nejsou odehrané žádné zápasy.</h3>`;
+    } else {
+        container.innerHTML = elements.join("\n");
+    }
+}
+
+
+loadTeams().then(() => console.log("teams loaded"))
+loadShooters().then(() => console.log("shooters loaded"))
