78515ad2f914b4e55ae3058d31090eaf993074f8
DoggoKt <mr.doge2019@gmail.com>
2025-01-17 10:13:44 +0100

    Fix shooters + add sort-by-column functionality to all tables

--- tabulka/tabulka.js
+++ tabulka/tabulka.js
@@ -23,36 +23,21 @@ const SHOOTER_TEMPLATE = `
 const GREEN_MAX = 4
 const YELLOW_MAX = 12
 
-async function loadTeams() {
-    const data = await fetch(FIREBASE_BASEURL + "getTeams").then(r => r.json());
-
-
-    const elements = Object.values(data).sort((a,b) => (b.points - a.points) || (b.goalsGiven-b.goalsReceived) - (a.goalsGiven-a.goalsReceived)).map((team, idx)=> {
-        return TEAM_TEMPLATE
-            .replaceAll("{POS}", String(idx + 1))
-            .replaceAll("{COLOR}", (idx+1) <= GREEN_MAX ? "green" : (idx+1) <= YELLOW_MAX ? "yellow" : "red")
-            .replaceAll("{TEAM}", team.name)
-            .replaceAll("{COUNT}", team.playedCount)
-            .replaceAll("{WON}", team.winCount)
-            .replaceAll("{LOST}", team.loseCount)
-            .replaceAll("{DRAWN}", team.drawCount)
-            .replaceAll("{GOALS}", `${team.goalsGiven}:${team.goalsReceived}`)
-            .replaceAll("{POINTS}", team.points);
-    });
-
-    const container = document.querySelector("#table-container .table.teams .body");
-
-    if (elements.length === 0) {
-        container.innerHTML = `<h3 style="text-align: center">Ještě nejsou odehrané žádné zápasy.</h3>`;
-    } else {
-        container.innerHTML = elements.join("\n");
+function reverseChildren(parent){
+    for (let i = 1; i < parent.childNodes.length; i++){
+        parent.insertBefore(parent.childNodes[i], parent.firstChild);
     }
 }
 
+let teams = {};
+async function loadTeams() {
+    teams = await fetch(FIREBASE_BASEURL + "getTeams").then(r => r.json());
+    sort("teams", (a,b) => (b.points - a.points) || ((b.goalsGiven-b.goalsReceived) - (a.goalsGiven-a.goalsReceived)))
+}
+
+let shooters = {}
 async function loadShooters() {
     const data = await fetch(FIREBASE_BASEURL + "getMatches").then(r => r.json());
-
-    const counted = {};
 
     for (const match of Object.values(data)){
         if (!match.events) continue;
@@ -61,37 +46,112 @@ async function loadShooters() {
             if (!event.includes("⚽")) continue;
             const splitted = event.split("(").map(e => e.replaceAll("⚽", "").trim())
 
-            console.log(match)
-            counted[splitted[0]] ??= {goals: 0, assists: 0/*, team: match.events.left.includes(event) ? match.team_left : match.team_right*/}
-            counted[splitted[0]].goals++;
+            shooters[splitted[0]] ??= {name: splitted[0], goals: 0, assists: 0/*, team: match.events.left.includes(event) ? match.team_left : match.team_right*/}
+            shooters[splitted[0]].goals++;
 
             if (splitted.length > 1) {
                 const assistName = splitted[1].slice(0,-1);
-                counted[assistName] ??= {goals: 0, assists: 0}
-                counted[assistName].assists++
+                shooters[assistName] ??= {name: assistName, goals: 0, assists: 0}
+                shooters[assistName].assists++
             }
         }
     }
 
-    console.log(counted)
+    sort("shooters", (a,b) => (b.goals - a.goals) || (b.assists - a.assists), true);
+}
 
-    const elements = Object.keys(counted).sort((a,b) => (counted[b].goals - counted[a].goals) || (counted[b].assists - counted[a].assists)).slice(0,30).map((plr, idx) => {
-      return SHOOTER_TEMPLATE
-          .replaceAll("{POS}", String(idx+1))
-          .replaceAll("{SHOOTER}", `${plr}`)
-          .replaceAll("{GOALS}", counted[plr].goals)
-          .replaceAll("{ASSISTS}", counted[plr].assists)
-    })
+// whichTable = "shooters"  || "teams"
+function sort(whichTable, sortFunction, calculatePositions = false){
+    const selector = `#table-container .table.${whichTable} .body`;
+    let elements;
 
-    const container = document.querySelector("#table-container .table.shooters .body");
+    switch (whichTable){
+        case "shooters":
+            let toBeMapped = Object.values(shooters).sort(sortFunction)
+            if (!calculatePositions) toBeMapped = toBeMapped.slice(0,30)
+            elements = toBeMapped.map((plr, idx) => {
+
+                if (!plr.position) {
+                    plr.position = idx+1;
+                    shooters[plr.name].position = plr.position;
+                }
+                return SHOOTER_TEMPLATE
+                    .replaceAll("{POS}", String(plr.position))
+                    .replaceAll("{SHOOTER}", plr.name)
+                    .replaceAll("{GOALS}", plr.goals)
+                    .replaceAll("{ASSISTS}", plr.assists)
+            })
+            break;
+        case "teams":
+            elements = Object.values(teams).sort(sortFunction).map((team, idx)=> {
+                if (!team.position) {
+                    team.position = idx + 1;
+                    teams[team.name].position = team.position;
+                }
+                if (!team.color) {
+                    team.color = (idx+1) <= GREEN_MAX ? "green" : (idx+1) <= YELLOW_MAX ? "yellow" : "red"
+                    teams[team.name].color = team.color;
+                }
+
+                return TEAM_TEMPLATE
+                    .replaceAll("{POS}", String(team.position))
+                    .replaceAll("{COLOR}", String(team.color))
+                    .replaceAll("{TEAM}", team.name)
+                    .replaceAll("{COUNT}", team.playedCount)
+                    .replaceAll("{WON}", team.winCount)
+                    .replaceAll("{LOST}", team.loseCount)
+                    .replaceAll("{DRAWN}", team.drawCount)
+                    .replaceAll("{GOALS}", `${team.goalsGiven}:${team.goalsReceived}`)
+                    .replaceAll("{POINTS}", team.points);
+            });
+            break;
+        default:
+            throw Error("Invalid sorting call.")
+    }
+    const container = document.querySelector(selector);
 
     if (elements.length === 0) {
         container.innerHTML = `<h3 style="text-align: center">Ještě nejsou odehrané žádné zápasy.</h3>`;
     } else {
         container.innerHTML = elements.join("\n");
     }
-}
 
+
+}
 
 loadTeams().then(() => console.log("teams loaded"))
 loadShooters().then(() => console.log("shooters loaded"))
+
+document.querySelectorAll(`#table-container .table th[data-property]`).forEach(el => el.addEventListener("click", _ => {
+    const table = el.closest("table");
+    // DEFAULT ASCENDING
+    const sortByProperty = (a,b) => (a[el.dataset.property] - b[el.dataset.property]) || (a.position - b.position)
+
+    const tableType = table.className.includes("teams") ? "teams" : "shooters";
+    if (el.dataset.property){
+        let sortDirection;
+        switch (el.innerText.slice(0,2)){
+            case "▼ ":
+                el.innerText = "▲ " + el.innerText.slice(2)
+                sortDirection = "asc";
+                break;
+            case "▲ ":
+                el.innerText = "▼ " + el.innerText.slice(2);
+                sortDirection = "dsc"
+                break;
+            default:
+                el.innerText = "▲  " + el.innerText.slice();
+                sortDirection = "asc"
+                break;
+        }
+
+        const previouslySorted = table.querySelector("th.sorted")
+        if (previouslySorted && previouslySorted !== el) {
+            previouslySorted.classList.remove("sorted");
+            previouslySorted.innerText = previouslySorted.innerText.slice(2);
+        }
+        if (!el.classList.contains("sorted")) el.classList.add("sorted")
+
+        sort(tableType, (a,b) => sortDirection === "asc" ? sortByProperty(a,b) : sortByProperty(b,a))
+    }
+}));
